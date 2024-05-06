f80836da71af165084a15c3df037bd7b72ed11e0
Stephen Ochieng <materiamystic@gmail.com>
2024-05-06 05:06:35 +0300

    Adds POST feature

--- src/pages/BotSpecs.js
+++ src/pages/BotSpecs.js
@@ -1,20 +1,42 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import YourBotArmy from "../components/YourBotArmy";
 
 function BotSpecs() {
   const [bot, setBot] = useState({});
   const params = useParams();
   const botId = params.id;
+  const [enlist, setEnlist] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:3000/bots/${botId}`)
       .then((r) => r.json())
       .then((bot) => setBot(bot))
-      .catch((error) => console.error(error));
+      .catch((error) => alert(error));
   }, [botId]);
+
+  function handleEnlist(bot) {
+    const duplicate = enlist.filter((item) => item.id === bot.id);
+
+    if (duplicate.length === 0) {
+      fetch("http://localhost:3000/enlisted", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bot),
+      })
+        .then((res) => res.json())
+        .then((postedBot) =>
+          setEnlist((prevEnlist) => [...prevEnlist, postedBot]),
+        )
+        .catch((error) => alert(error.message));
+    }
+  }
 
   return (
     <div className="container">
+      <header className="App-header">
+        <YourBotArmy />
+      </header>
       <div className="row">
         <div class="col-6">
           <img src={bot.avatar_url} alt="bot_avatar" className="float-end" />
@@ -38,9 +60,16 @@ function BotSpecs() {
             <em className="m-1">{bot.armor}</em>
           </div>
           <Link to="/">
-            <button className="d-block w-50 mt-2">Go Back</button>
+            <button className="d-block w-50 mt-2 rounded">Go Back</button>
           </Link>
-          <button className="mt-2 w-50">Enlist</button>
+          <Link to="/">
+            <button
+              className="mt-2 w-50 rounded"
+              onClick={() => handleEnlist(bot)}
+            >
+              Enlist
+            </button>
+          </Link>
         </div>
       </div>
     </div>
